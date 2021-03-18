d800d11f63a5ac253d93d5c8899b62d7b9a77ad3
Siddharthan <sidtypist@gmail.com>
2021-03-18 14:56:50 +0530

    as same as previous comment

--- src/components/HomePage/CategoryBox/CategoryBox.jsx
+++ src/components/HomePage/CategoryBox/CategoryBox.jsx
@@ -2,53 +2,42 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Category from "./Category";
 import catItem from "../../../categorydata";
-
+import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-
-
 
 function CategoryBox(ctItem) {
   return (
-
-      
-<Grid item  class="category-card" sm={3} md={2}>
-<Category 
+    <Grid item class="category-card" sm={3} md={2}>
+      <Category
         key={ctItem.id}
         iconName={ctItem.iconName}
         catName={ctItem.catName}
       />
-</Grid>
-    
-  )};
+    </Grid>
+  );
+}
 
-  function Categories(){
-    
-    return(
-
-      <Container maxWidth="lg" className="categoryBox">
+function Categories() {
+  return (
+    <Container maxWidth="lg" className="categoryBox">
       <input
         type="text"
         name="search"
         placeholder="Search for a service e.g. Plumbing"
       />
 
-
-
-      <Grid container justify="center"
-  alignItems="center"
-  xs={12} spacing={1}
-  className="catItem">
-     {catItem.map(CategoryBox)}
-      
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        xs={12}
+        spacing={1}
+        className="catItem"
+      >
+        {catItem.map(CategoryBox)}
       </Grid>
-      
-    
-      
-    
-      </Container>
-
-    )
-
-  }
+    </Container>
+  );
+}
 
 export default Categories;
